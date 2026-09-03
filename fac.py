fbdbc4d0a090ecb775974ce1b7935bdf7cd16fde
Diya-qwe <diyasree.s2024@vitstudent.ac.in>
2026-09-03 07:28:19 +0530

    cc

--- fac.py
+++ fac.py
@@ -1,5 +1,8 @@
-n = int(input("Enter a number: "))
+import sys
+
+# If a number is provided as a command‑line argument, use it; otherwise default to 5
+n = int(sys.argv[1]) if len(sys.argv) > 1 else 5
 fact = 1
 for i in range(1, n + 1):
-    fact = fact * i
-print("Factorial =", fact)
+    fact *= i
+print(f"Factorial of {n} is {fact}")
